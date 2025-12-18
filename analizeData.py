c0d30481e2e862bf44212fb7a00ec939d951573a
ALENOR <ilyalepa333@gmail.com>
2025-12-18 23:51:39 +0300

    I started development voice assistant for my ecosystem

---
+++ analizeData.py
@@ -0,0 +1,12 @@
+import speech_recognition as speech
+
+
+dictCommands = {
+    "StartWord": ["спектрум", "spectrum"],
+    "Commands": ["YouTube", "BK"]
+}
+
+
+def analizeData(response):
+    return response
+
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import speech_recognition as speech
+import webbrowser
 
 
 core = speech.Recognizer()
 
 dictCommands = {
-    "StartWord": ["спектрум", "spectrum"],
-    "Commands": ["YouTube", "BK"]
+    "Commands": ["YouTube", "ВК"],
+    "StartWords": ["Открой"]
 }
 
 
@@ -15,18 +16,29 @@ def engineSpeech():
         audio = core.listen(micro)
     try:
         result = core.recognize_google(audio, language="ru-RU")
-        analizeData(result)
+        response = analizeData(result)
+        return response
     except speech.UnknownValueError:
         print("Не удалось распознать речь")
     except speech.RequestError as e:
         print(e)
 
 def analizeData(response):
-    print(response)
+    formattedText = response.split(" ")
+    if formattedText[0] in dictCommands["StartWords"]:
+        startActivity(formattedText[1])
+    else:
+        print("dd")
+
+def startActivity(activity):
+    if activity == dictCommands["Commands"][0]:
+        webbrowser.open_new_tab("https://www.youtube.com")
+    elif activity == dictCommands["Commands"][1]:
+        webbrowser.open_new_tab("https://vk.com/feed")
+    else:
+        return "Извините, но я не смогу выполнить"
 
 
-    
-        
 
 while True:
     engineSpeech()
